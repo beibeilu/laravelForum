6a2c6fb501f1c9ee44cd146961bc1acaf2d55e3f
Beibei Lu <pamela.lu.beibei@gmail.com>
2017-05-19 00:31:53 -0400

    add select-channel to create new thread.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,13 +60,18 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
-                            <li><a href="{{'/threads/create'}}">Create New Thread</a></li>
+
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     Welcome, {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
+
+
+                                    <li><a href="{{'/threads/create'}}">Create New Thread</a></li>
+                                    <li role="separator" class="divider"></li>
+
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
